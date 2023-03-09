69d762652848a679b63cd047be76b8bdf9fcd096
Rohan G <gvrohan11@gmail.com>
2023-03-08 18:28:15 -0600

    added variables and grid in c++

---
+++ bot_code/high_level/variables.h
@@ -0,0 +1,30 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H
+
+using namespace std;
+
+#include <vector>
+
+// Variables
+
+// Game params
+extern vector<int> PACBOT_STARTING_POSITION = {14, 7};
+
+// Cherry param
+extern vector<int> CHERRY_POS = {13, 13};
+
+// Grid Parameters
+extern int I = 1;
+extern int o = 2;
+extern int e = 3;
+extern int O = 4;
+extern int n = 5;
+
+// Points
+extern int CHERRY_POINTS = 100;
+extern int POWER_POINTS = 50;
+extern int PELLET_POINTS = 10;
+// int GHOST_POINTS = 200;
+
+
+#endif
