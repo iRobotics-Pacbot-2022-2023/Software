bd96283cfd6b82140cfdb40831bcd4fb83f368af
Rohan G <gvrohan11@gmail.com>
2023-03-27 18:44:33 -0500

    files

---
+++ bot_code/high_level/Minimax.h
@@ -0,0 +1,14 @@
+#pragma once
+
+using namespace std;
+
+#include "variables.h"
+#include "grid.h"
+
+class Minimax {
+    public:
+
+    private:
+        vector<vector<int>> grid = grid::grid;
+
+};
--- bot_code/high_level/variables.h
+++ bot_code/high_level/variables.h
@@ -13,11 +13,12 @@ const vector<int> PACBOT_STARTING_POSITION = {14, 7};
 const vector<int> CHERRY_POS = {13, 13};
 
 // Grid Parameters
-const int I = 1;
-const int o = 2;
-const int e = 3;
-const int O = 4;
-const int n = 5;
+const int I = 1; // WALL
+const int o = 2; // Pellet
+const int e = 3; // unreachable
+const int O = 4; // power point
+const int n = 5; // untouchable
+const int P = 6; // Pacbot
 
 // Points
 const int CHERRY_POINTS = 100;
