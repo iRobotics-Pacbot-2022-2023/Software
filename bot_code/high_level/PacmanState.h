847cd4e955c14a88869a39ef1e6347d973a9a692
kyle11067 <49108058+kyle11067@users.noreply.github.com>
2023-04-08 17:04:38 -0500

    Change incrementers to setters. 4/8/2023, 5:04 PM

--- bot_code/high_level/PacmanState.h
+++ bot_code/high_level/PacmanState.h
@@ -15,25 +15,28 @@
 class PacmanState {
 private:
     int score;
-    std::pair<int, int> pos;
+    static std::pair<int, int> botPos;
     int lives;
-    int level;
+    // int level;
     bool gameover = false;
     //red, orange, blue, pink
-                                                              //
+    //
 
-    int power_up_weight = 50;
+    // int power_up_weight = 50;
     int power_ups_left; //power ups remaining
 
-    int pellet_weight = 10;
+    // int pellet_weight = 10;
     int pellets_left;
     // int ghost_score;
     enum direction {
         right, up, left, down, upperRight, upperLeft, lowerRight, lowerLeft
     };
     direction direction_facing;
-    bool chase = true;
-    bool scatter = false;
+    // bool chase = true;
+    // bool scatter = false;
+    void updatePelletsleft(int pelletLeft);
+    int updatePowerupsleft(int powerUpsLeft) ;
+    
     // void moveRedChase();
     // void moveOrangeChase();
     // void moveBlueChase();
@@ -57,33 +60,26 @@ public:
 
     std::pair<int, int> getBotPos();
 
-    int getX();
-    int getY();
+    // int getX();
+    // int getY();
 
     void changeX(int new_x);
     void changeY(int new_y);
-    void increaseScore(int points) {
-        score += points;
-    }
+    void updateScore(int points);
+    void updateLevel(int levels);
+    void decreaseLives() ;
 
-    void decreaseLives() {
-        lives--;
-        if (lives == 0) {
-            gameover = true;
-        }
-    }
+    // void nextLevel() {
+    //     level++;
+    // }
 
-    void nextLevel() {
-        level++;
-    }
+    // void gameOver() {
+    //     gameover = true;
+    // }
 
-    void gameOver() {
-        gameover = true;
-    }
-
-    bool isGameOver() {
-        return gameover;
-    }
+    // bool isGameOver() {
+    //     return gameover;
+    // }
 
     int getScore() {
         return score;
@@ -104,12 +100,8 @@ public:
     }
     double get_euclidian_distance(std::pair<int, int> pos_a, std::pair<int, int> pos_b);
     std::vector<std::pair<int, int>> find_possible_moves();
-    bool is_move_legal(std::pair<int, int> move);
+    bool is_move_legal(std::pair<int, int> move, char[][]grid);
 
     void movePlayer(double dx, double dy);
     bool hasCollided();
-    void moveOrange();
-    void moveRed();
-    void movePink();
-    void moveBlue();
 };
