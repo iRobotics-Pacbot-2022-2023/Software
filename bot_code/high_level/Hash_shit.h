fdc03cfee5da1fa1c1be76c65327f28f2fee9210
Timo <timofong567@gmail.com>
2023-04-22 17:14:45 -0500

    Hashing testing and it works

--- bot_code/high_level/Hash_shit.h
+++ bot_code/high_level/Hash_shit.h
@@ -4,8 +4,7 @@
 #include <random>
 #include <list> 
 using namespace std;
-//Important stuff of the Zoribist hashing implementation
 unsigned long long randomNum();
 void tablefiller();
 unsigned long long hashgenerator(vector<vector<int>> grid); 
-unsigned long long ZorbTable [28][31][7]; 
+
--- bot_code/high_level/test.cpp
+++ bot_code/high_level/test.cpp
@@ -1,10 +1,10 @@
 #include "Ghost.h"
 #include <iostream>
-
+#include "Hash_shit.h"
 int main () {
-    // PacBot pac;
+    //PacBot pac;
     // std::cout << pac.getPos().first << " " << pac.getPos().second << std::endl;
-    // Ghost ghost();
+    //Ghost ghost(pac);
     // std::cout << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << std::endl;
     // std::cout << ghost.is_move_legal(std::pair<int, int>(0,0)) << std::endl;
     // std::cout << ghost._get_next_pink_chase_move().first.first << " " << ghost._get_next_pink_chase_move().first.second << std::endl;
@@ -38,18 +38,56 @@ int main () {
 
 
 
-    ghost.ghostMove(std::pair<int, int>(1,1));
+    //ghost.ghostMove(std::pair<int, int>(1,1));
     // std::cout << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << std::endl;
     // std::cout << pac.getPos().first << " " << pac.getPos().second << std::endl;
-
-    // (ghost.getGhostLocation().first != pac.getPos().first) || (ghost.getGhostLocation().second != pac.getPos().second)
-    int count = 100;
-    while (count > 0) {
-        std::cout << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << " " << ghost.get_euclidian_distance(ghost.getGhostLocation(), pac.getPos()) << std::endl;
+    //while ((ghost.getGhostLocation().first != pac.getPos().first) || (ghost.getGhostLocation().second != pac.getPos().second)) {
+        //std::cout << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << std::endl;
         // std::cout << ghost._get_next_red_chase_move().first.first << " " << ghost._get_next_red_chase_move().first.second << std::endl;
-        std::pair<int, int> newMove(ghost._get_next_orange_chase_move().first.first, ghost._get_next_orange_chase_move().first.second);
-        ghost.ghostMove(newMove);
-        --count;
-    }
+        //std::pair<int, int> newMove(ghost._get_next_red_chase_move().first.first, ghost._get_next_red_chase_move().first.second);
+        //ghost.ghostMove(newMove);
+    //}
+
+    // this is for testing the hashing 
+    //run g++ test.cpp Hashing_shit.cpp -o testing
+    // then run the exe file
+    vector<vector<int>> grid =  {{I,I,I,I,I,I,I,I,I,I,I,I,e,e,e,I,v,I,e,e,e,I,I,I,I,I,I,I,I,I,I}, // 0
+                                        {I,o,o,o,o,I,I,O,o,o,o,I,e,e,e,I,v,I,e,e,e,I,o,o,o,o,o,O,o,o,I},
+                                        {I,o,I,I,o,I,I,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,o,o,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,I,I,I,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,I,I,I,I,I,o,I,I,I,I,I,v,I,I,I,I,I,o,I,I,o,I,I,I,o,I}, // 5
+                                        {I,o,I,I,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,I},
+                                        {I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,v,I,I,I,I,I,I,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,v,I,I,I,I,I,I,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,o,o,o,I,I,o,v,v,v,v,v,v,v,v,v,I,I,o,o,o,o,I,I,I,o,I},
+                                        {I,o,I,I,o,I,I,o,I,I,o,I,I,v,I,I,I,I,I,v,I,I,o,I,I,o,I,I,I,o,I}, // 10
+                                        {I,o,I,I,o,I,I,o,I,I,o,I,I,v,I,n,n,n,I,v,I,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,o,o,o,I,I,o,o,o,o,I,I,v,I,n,n,n,I,v,v,v,o,I,I,o,o,o,o,o,I},
+                                        {I,o,I,I,I,I,I,v,I,I,I,I,I,v,I,n,n,n,n,v,I,I,I,I,I,o,I,I,I,I,I},
+                                        {I,o,I,I,I,I,I,P,I,I,I,I,I,v,I,n,n,n,n,v,I,I,I,I,I,o,I,I,I,I,I},
+                                        {I,o,o,o,o,I,I,o,o,o,o,I,I,v,I,n,n,n,I,v,v,v,o,I,I,o,o,o,o,o,I}, // 15
+                                        {I,o,I,I,o,I,I,o,I,I,o,I,I,v,I,n,n,n,I,v,I,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,I,I,o,I,I,o,I,I,v,I,I,I,I,I,v,I,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,o,o,o,I,I,o,v,v,v,v,v,v,v,v,v,I,I,o,o,o,o,I,I,I,o,I},
+                                        {I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,v,I,I,I,I,I,I,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,v,I,I,I,I,I,I,I,I,o,I,I,I,o,I}, // 20
+                                        {I,o,I,I,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,I},
+                                        {I,o,I,I,o,I,I,I,I,I,o,I,I,I,I,I,v,I,I,I,I,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,I,I,I,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,o,o,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,I,I,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I}, // 25
+                                        {I,o,o,o,o,I,I,O,o,o,o,I,e,e,e,I,v,I,e,e,e,I,o,o,o,o,o,O,o,o,I},
+                                        {I,I,I,I,I,I,I,I,I,I,I,I,e,e,e,I,v,I,e,e,e,I,I,I,I,I,I,I,I,I,I}};
+                               //        |         |         |         |         |         |         |
+                               //        0         5         10        15       20         25        30 
+    tablefiller(); 
+    unsigned long long grit = hashgenerator(grid); 
+    //initial grid state
+    cout << "Hash Value: " << grit << std::endl; 
+    //if we change the grid state
+    int piece = grid[22][14]; 
+    grid[22][14] = v; 
+    ///grit ^= ZorbTable[22][14][piece]; 
     return 0;
 }
