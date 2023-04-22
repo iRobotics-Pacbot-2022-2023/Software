17a09ecd4305635da4e42f14c55b3e5f3db9786a
kyle11067 <49108058+kyle11067@users.noreply.github.com>
2023-04-21 22:00:03 -0500

    Fix errors by giving the grid vector a name

--- bot_code/high_level/PacmanState.h
+++ bot_code/high_level/PacmanState.h
@@ -54,7 +54,7 @@ public:
     }
     //************************
                                  // Bottom Left
-           vector<vector<int>>  =  {{I,I,I,I,I,I,I,I,I,I,I,I,e,e,e,I,v,I,e,e,e,I,I,I,I,I,I,I,I,I,I}, // 0
+           vector<vector<int>> grid  =  {{I,I,I,I,I,I,I,I,I,I,I,I,e,e,e,I,v,I,e,e,e,I,I,I,I,I,I,I,I,I,I}, // 0
                                         {I,o,o,o,o,I,I,O,o,o,o,I,e,e,e,I,v,I,e,e,e,I,o,o,o,o,o,O,o,o,I},
                                         {I,o,I,I,o,I,I,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
                                         {I,o,I,I,o,o,o,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
