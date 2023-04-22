a19ed31d20c9af4d91f4adb478cd1a93fca29948
rudrapt2 <rudrapt2@illinois.edu>
2023-04-21 23:38:48 -0500

    got it working except for last part in loop

--- bot_code/low_level/Arduino_Code/Arduino_Code.ino
+++ bot_code/low_level/Arduino_Code/Arduino_Code.ino
@@ -25,8 +25,8 @@
 #define imu_i2c_addr 0x4A
 
 
-Adafruit_BNO08x imu();
-sh2_sensorValue_t sensorValue;
+Adafruit_BNO08x imu(-1);
+sh2_SensorValue_t sensorValue;
 
 VL53L4CX tof1(&DEV_I2C, 16);
 VL53L4CX tof2(&DEV_I2C, 15);
@@ -35,18 +35,22 @@ VL53L4CX tof4(&DEV_I2C, 13);
 
 
 void i2cSetup(){
-  DEV_I2C.setSDA(18);
-  DEV_I2C.setSCL(19);
+  // DEV_I2C.setSDA(18);
+  // DEV_I2C.setSCL(19);
+  Serial.println("starting i2c setup");
   DEV_I2C.begin();
+  Serial.println("done with i2c");
 }
 
 void imuSetup(){
-  while(!imu.begin_I2C(imu_i2c_addr, &Dev_I2C, 0)){
+  while(!imu.begin_I2C(imu_i2c_addr, &DEV_I2C, 0)){
     delay(10);
   }
 
   imuSetReports();
   delay(100);
+
+  Serial.println("done withimu setup");
 }
 
 float imuLoop(Adafruit_BNO08x imu){
@@ -55,15 +59,16 @@ float imuLoop(Adafruit_BNO08x imu){
   }
 
   if (!imu.getSensorEvent(&sensorValue)){
-    return;
+    return 1000;
   }
 
   if (sensorValue.sensorId == SH2_GYROSCOPE_CALIBRATED){
     return sensorValue.un.gyroscope.z;
   }
+  return 0;
 }
 
-void imuSetReports(void) {
+void imuSetReports() {
   imu.enableReport(SH2_GYROSCOPE_CALIBRATED);
 }
 
@@ -106,54 +111,102 @@ void tofSetup(){
   // tof3.begin(tof3_i2c_addr);
   // tof4.begin(tof4_i2c_addr);
 
+  Serial.println("Starting tof starting");
+
   tof1.VL53L4CX_StartMeasurement();
   tof2.VL53L4CX_StartMeasurement();
   tof3.VL53L4CX_StartMeasurement();
   tof4.VL53L4CX_StartMeasurement();
+
+  Serial.println("Done with tof setup");
 }
 
-float tofLoop(tof){
+float tofLoop(VL53L4CX tof){
+  Serial.println("starting tofloop");
+
   VL53L4CX_MultiRangingData_t MultiRangingData;
   VL53L4CX_MultiRangingData_t *pMultiRangingData = &MultiRangingData;
   uint8_t NewDataReady = 0;
-  int no_of_object_found = 0, j;
-  char report[64];
-  int status;
+  int no_of_object_found = 0;
+  int status = 0;
   
+  Serial.println("got to this point");
+
   status = tof.VL53L4CX_GetMeasurementDataReady(&NewDataReady);
 
+  Serial.println("got to the second point");
   if ((!status) && (NewDataReady != 0)) {
+    Serial.println("entered here");
     status = tof.VL53L4CX_GetMultiRangingData(pMultiRangingData);
     no_of_object_found = pMultiRangingData->NumberOfObjectsFound;
-    int max = 99999999999;
-    for (int i = 0; i < no_of_object_found){
-      if(pMultiRangingData->RangeData[i].RangeMillimeter <= max){
-        max = pMultiRangingData->RangeData[i].RangeMillimeter;
+    float max = 99999;
+    Serial.println(no_of_object_found);
+    for (int i = 0; i < no_of_object_found; i++){
+      if(pMultiRangingData->RangeData[i].RangeMilliMeter <= max){
+        max = pMultiRangingData->RangeData[i].RangeMilliMeter;
       }
     }
     if (status == 0) {
-      status = sensor_vl53l4cx_sat.VL53L4CX_ClearInterruptAndStartMeasurement();
+      status = tof.VL53L4CX_ClearInterruptAndStartMeasurement();
     }
+    Serial.printf("%f", max);
     return max;
   }
+  Serial.println("reached end");
+  return -1;
 }
 
-void setup() {
+void setup(void) {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  while(!Serial) delay(10);
+  Serial.println("hi");
   i2cSetup();
   tofSetup();
   imuSetup();
   
 }
 
+
+float tof1_distance = 0;
+float tof2_distance = 0;
+float tof3_distance = 0;
+float tof4_distance = 0;
+float imu_angle = 0;
+float temp;
 void loop() {
   // put your main code here, to run repeatedly:
-  tof1_distance = tofLoop(tof1);
-  tof2_distance = tofLoop(tof2);
-  tof3_distance = tofLoop(tof3);
-  tof4_distance = tofLoop(tof4);
-  imu_angle = imuLoop(imu);
-
   
+  temp = tofLoop(tof1);
+  if(temp != -1){
+    tof1_distance = temp;
+  }
+
+
+  temp = tofLoop(tof2);
+  if(temp != -1){
+    tof2_distance = temp;
+  }
+
+  temp = tofLoop(tof3);
+  if(temp != -1){
+    tof3_distance = temp;
+  }
+
+  temp = tofLoop(tof4);
+  if(temp != -1){
+    tof4_distance = temp;
+  }
+
+  // temp = imuLoop(imu);
+  // if(temp != 1000){
+  //   imu_angle = temp;
+  // }
+
+  Serial.printf("%f", tof1_distance);
+  Serial.printf("%f", tof2_distance);
+  Serial.printf("%f", tof3_distance);
+  Serial.printf("%f", tof4_distance);
+  Serial.printf("%d", imu_angle);
 
 }
