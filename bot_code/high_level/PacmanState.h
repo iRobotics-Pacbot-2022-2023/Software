f809815ce1ce9d49a915986794c5dcbbeb081891
Tamir Battsogt <angutamir@gmail.com>
2023-04-24 19:11:22 -0500

    b

--- bot_code/high_level/PacmanState.h
+++ bot_code/high_level/PacmanState.h
@@ -17,83 +17,16 @@ class PacmanState {
     enum Direction {
             right, up, left, down
         };    
-    
-private:
-
-    // int score;
-    std::pair<int, int> pos;
-    // int lives;
-
-    bool gameover = false;
-    // *************************  Data for pellets, powerups and cherries
-    int pellets_left;
-    int pellets_eaten;
-    int cherries_eaten;
-    int prev_powerups_eaten; // update after we change state (+1) (POWERUP -> FREIGHTENED)
-    int curr_powerups_eaten; // update after we eat a powerup (+1)
-    //***************************
-  // cherry time
-    
-    //********************************** Direction and grid
-    
-    Direction direction_facing;
-    void updateGrid(vector<vector<int>> new_grid) { grid = new_grid; } // GET FROM SYSTEM
-    vector<vector<int>> getGrid() { return grid; }
-   //*********************/
-    // Position, gameover, pellets/cherries/powerups, direction_facing, grid
-public:
     //************************ Constructor
     PacmanState();
-    PacmanState(std::pair<int, int> botPos) {
-        // score = 0; // retrieve from server
-        // lives = 3; // retrieve from server
-        gameover = false; // retrieve from server
-        pos = botPos; // retrieve from server
-        pellets_left = 0; // retrieve from server
-        pellets_eaten = 0; // retrieve from server
-        cherries_eaten = 0; // retrieve from server
-        prev_powerups_eaten = 0; // retrieve from server
-        curr_powerups_eaten = 0; // retrieve from server
-    }
+    PacmanState(std::pair<int, int> botPos, PacmanState::Direction pacDirection, std::vector<std::vector<int>> grid, int pelletsLeft, int pellets_eat, int cherries_eat, int prev_powerups, int curr_powerup);
     
-    //************************
-                                 // Bottom Left
-           vector<vector<int>> grid  =  {{I,I,I,I,I,I,I,I,I,I,I,I,e,e,e,I,v,I,e,e,e,I,I,I,I,I,I,I,I,I,I}, // 0
-                                        {I,o,o,o,o,I,I,O,o,o,o,I,e,e,e,I,v,I,e,e,e,I,o,o,o,o,o,O,o,o,I},
-                                        {I,o,I,I,o,I,I,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
-                                        {I,o,I,I,o,o,o,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
-                                        {I,o,I,I,o,I,I,I,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
-                                        {I,o,I,I,o,I,I,I,I,I,o,I,I,I,I,I,v,I,I,I,I,I,o,I,I,o,I,I,I,o,I}, // 5
-                                        {I,o,I,I,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,I},
-                                        {I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,v,I,I,I,I,I,I,I,I,o,I,I,I,o,I},
-                                        {I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,v,I,I,I,I,I,I,I,I,o,I,I,I,o,I},
-                                        {I,o,I,I,o,o,o,o,I,I,o,v,v,v,v,v,v,v,v,v,I,I,o,o,o,o,I,I,I,o,I},
-                                        {I,o,I,I,o,I,I,o,I,I,o,I,I,v,I,I,I,I,I,v,I,I,o,I,I,o,I,I,I,o,I}, // 10
-                                        {I,o,I,I,o,I,I,o,I,I,o,I,I,v,I,n,n,n,I,v,I,I,o,I,I,o,I,I,I,o,I},
-                                        {I,o,o,o,o,I,I,o,o,o,o,I,I,v,I,n,n,n,I,v,v,v,o,I,I,o,o,o,o,o,I},
-                                        {I,o,I,I,I,I,I,v,I,I,I,I,I,v,I,n,n,n,n,v,I,I,I,I,I,o,I,I,I,I,I},
-                                        {I,o,I,I,I,I,I,P,I,I,I,I,I,v,I,n,n,n,n,v,I,I,I,I,I,o,I,I,I,I,I},
-                                        {I,o,o,o,o,I,I,o,o,o,o,I,I,v,I,n,n,n,I,v,v,v,o,I,I,o,o,o,o,o,I}, // 15
-                                        {I,o,I,I,o,I,I,o,I,I,o,I,I,v,I,n,n,n,I,v,I,I,o,I,I,o,I,I,I,o,I},
-                                        {I,o,I,I,o,I,I,o,I,I,o,I,I,v,I,I,I,I,I,v,I,I,o,I,I,o,I,I,I,o,I},
-                                        {I,o,I,I,o,o,o,o,I,I,o,v,v,v,v,v,v,v,v,v,I,I,o,o,o,o,I,I,I,o,I},
-                                        {I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,v,I,I,I,I,I,I,I,I,o,I,I,I,o,I},
-                                        {I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,v,I,I,I,I,I,I,I,I,o,I,I,I,o,I}, // 20
-                                        {I,o,I,I,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,I},
-                                        {I,o,I,I,o,I,I,I,I,I,o,I,I,I,I,I,v,I,I,I,I,I,o,I,I,o,I,I,I,o,I},
-                                        {I,o,I,I,o,I,I,I,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
-                                        {I,o,I,I,o,o,o,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
-                                        {I,o,I,I,o,I,I,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I}, // 25
-                                        {I,o,o,o,o,I,I,O,o,o,o,I,e,e,e,I,v,I,e,e,e,I,o,o,o,o,o,O,o,o,I},
-                                        {I,I,I,I,I,I,I,I,I,I,I,I,e,e,e,I,v,I,e,e,e,I,I,I,I,I,I,I,I,I,I}};
-                               //        |         |         |         |         |         |         |
-                               //        0         5         10        15       20         25        30
+    void updateGrid(vector<vector<int>> new_grid); // GET FROM SYSTEM
+    vector<vector<int>> getGrid();
 
     //************************ Position getter and setter 
-    std::pair<int, int> getBotPos() { return pos; }
-    void changePos(std::pair<int, int> new_pos) {
-        pos = new_pos;
-    }
+    std::pair<int, int> getBotPos();
+    void changePos(std::pair<int, int> new_pos);
     //**************************
 
     //*******************Pellet getter and setter
@@ -148,8 +81,64 @@ public:
     double get_euclidian_distance(std::pair<int, int> pos_a, std::pair<int, int> pos_b);
     std::vector<std::pair<int, int>> find_possible_moves();
     bool is_move_legal(std::pair<int, int> move);
-    void movePlayer(int dx, int dy);
-    bool hasCollided();
+    void movePlayer(std::pair<int, int> move);
+    bool hasCollided(std::vector<std::pair<int, int>> ghostLocs);
     void introduceCherry();
     //************************
+private:
+
+    // int score;
+    std::pair<int, int> pos;
+    // int lives;
+
+    bool gameover = false;
+    // *************************  Data for pellets, powerups and cherries
+    int pellets_left;
+    int pellets_eaten;
+    int cherries_eaten;
+    int prev_powerups_eaten; // update after we change state (+1) (POWERUP -> FREIGHTENED)
+    int curr_powerups_eaten; // update after we eat a powerup (+1)
+    //***************************
+  // cherry time
+    
+    //********************************** Direction and grid
+    
+    Direction direction_facing;
+
+    std::vector<std::vector<int>> grid;
+    
+   //*********************/
+    // Position, gameover, pellets/cherries/powerups, direction_facing, grid
+    // //************************
+    //                              // Bottom Left
+        //    vector<vector<int>> grid  =  {{I,I,I,I,I,I,I,I,I,I,I,I,e,e,e,I,v,I,e,e,e,I,I,I,I,I,I,I,I,I,I}, // 0
+        //                                 {I,o,o,o,o,I,I,O,o,o,o,I,e,e,e,I,v,I,e,e,e,I,o,o,o,o,o,O,o,o,I},
+        //                                 {I,o,I,I,o,I,I,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
+        //                                 {I,o,I,I,o,o,o,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
+        //                                 {I,o,I,I,o,I,I,I,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
+        //                                 {I,o,I,I,o,I,I,I,I,I,o,I,I,I,I,I,v,I,I,I,I,I,o,I,I,o,I,I,I,o,I}, // 5
+        //                                 {I,o,I,I,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,I},
+        //                                 {I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,v,I,I,I,I,I,I,I,I,o,I,I,I,o,I},
+        //                                 {I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,v,I,I,I,I,I,I,I,I,o,I,I,I,o,I},
+        //                                 {I,o,I,I,o,o,o,o,I,I,o,v,v,v,v,v,v,v,v,v,I,I,o,o,o,o,I,I,I,o,I},
+        //                                 {I,o,I,I,o,I,I,o,I,I,o,I,I,v,I,I,I,I,I,v,I,I,o,I,I,o,I,I,I,o,I}, // 10
+        //                                 {I,o,I,I,o,I,I,o,I,I,o,I,I,v,I,n,n,n,I,v,I,I,o,I,I,o,I,I,I,o,I},
+        //                                 {I,o,o,o,o,I,I,o,o,o,o,I,I,v,I,n,n,n,I,v,v,v,o,I,I,o,o,o,o,o,I},
+        //                                 {I,o,I,I,I,I,I,v,I,I,I,I,I,v,I,n,n,n,n,v,I,I,I,I,I,o,I,I,I,I,I},
+        //                                 {I,o,I,I,I,I,I,P,I,I,I,I,I,v,I,n,n,n,n,v,I,I,I,I,I,o,I,I,I,I,I},
+        //                                 {I,o,o,o,o,I,I,o,o,o,o,I,I,v,I,n,n,n,I,v,v,v,o,I,I,o,o,o,o,o,I}, // 15
+        //                                 {I,o,I,I,o,I,I,o,I,I,o,I,I,v,I,n,n,n,I,v,I,I,o,I,I,o,I,I,I,o,I},
+        //                                 {I,o,I,I,o,I,I,o,I,I,o,I,I,v,I,I,I,I,I,v,I,I,o,I,I,o,I,I,I,o,I},
+        //                                 {I,o,I,I,o,o,o,o,I,I,o,v,v,v,v,v,v,v,v,v,I,I,o,o,o,o,I,I,I,o,I},
+        //                                 {I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,v,I,I,I,I,I,I,I,I,o,I,I,I,o,I},
+        //                                 {I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,v,I,I,I,I,I,I,I,I,o,I,I,I,o,I}, // 20
+        //                                 {I,o,I,I,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,I},
+        //                                 {I,o,I,I,o,I,I,I,I,I,o,I,I,I,I,I,v,I,I,I,I,I,o,I,I,o,I,I,I,o,I},
+        //                                 {I,o,I,I,o,I,I,I,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
+        //                                 {I,o,I,I,o,o,o,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
+        //                                 {I,o,I,I,o,I,I,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I}, // 25
+        //                                 {I,o,o,o,o,I,I,O,o,o,o,I,e,e,e,I,v,I,e,e,e,I,o,o,o,o,o,O,o,o,I},
+        //                                 {I,I,I,I,I,I,I,I,I,I,I,I,e,e,e,I,v,I,e,e,e,I,I,I,I,I,I,I,I,I,I}};
+    //                            //        |         |         |         |         |         |         |
+    //                            //        0         5         10        15       20         25        30
 };
--- bot_code/high_level/test.cpp
+++ bot_code/high_level/test.cpp
@@ -1,8 +1,54 @@
 #include "Ghost.h"
+#include "PacmanState.h"
 #include <iostream>
 
 int main () {
     // PacBot pac;
+    vector<vector<int>> grid  =  {{I,I,I,I,I,I,I,I,I,I,I,I,e,e,e,I,v,I,e,e,e,I,I,I,I,I,I,I,I,I,I}, // 0
+                                        {I,o,o,o,o,I,I,O,o,o,o,I,e,e,e,I,v,I,e,e,e,I,o,o,o,o,o,O,o,o,I},
+                                        {I,o,I,I,o,I,I,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,o,o,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,I,I,I,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,I,I,I,I,I,o,I,I,I,I,I,v,I,I,I,I,I,o,I,I,o,I,I,I,o,I}, // 5
+                                        {I,o,I,I,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,I},
+                                        {I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,v,I,I,I,I,I,I,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,v,I,I,I,I,I,I,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,o,o,o,I,I,o,v,v,v,v,v,v,v,v,v,I,I,o,o,o,o,I,I,I,o,I},
+                                        {I,o,I,I,o,I,I,o,I,I,o,I,I,v,I,I,I,I,I,v,I,I,o,I,I,o,I,I,I,o,I}, // 10
+                                        {I,o,I,I,o,I,I,o,I,I,o,I,I,v,I,n,n,n,I,v,I,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,o,o,o,I,I,o,o,o,o,I,I,v,I,n,n,n,I,v,v,v,o,I,I,o,o,o,o,o,I},
+                                        {I,o,I,I,I,I,I,v,I,I,I,I,I,v,I,n,n,n,n,v,I,I,I,I,I,o,I,I,I,I,I},
+                                        {I,o,I,I,I,I,I,P,I,I,I,I,I,v,I,n,n,n,n,v,I,I,I,I,I,o,I,I,I,I,I},
+                                        {I,o,o,o,o,I,I,o,o,o,o,I,I,v,I,n,n,n,I,v,v,v,o,I,I,o,o,o,o,o,I}, // 15
+                                        {I,o,I,I,o,I,I,o,I,I,o,I,I,v,I,n,n,n,I,v,I,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,I,I,o,I,I,o,I,I,v,I,I,I,I,I,v,I,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,o,o,o,I,I,o,v,v,v,v,v,v,v,v,v,I,I,o,o,o,o,I,I,I,o,I},
+                                        {I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,v,I,I,I,I,I,I,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,v,I,I,I,I,I,I,I,I,o,I,I,I,o,I}, // 20
+                                        {I,o,I,I,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,I},
+                                        {I,o,I,I,o,I,I,I,I,I,o,I,I,I,I,I,v,I,I,I,I,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,I,I,I,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,o,o,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I},
+                                        {I,o,I,I,o,I,I,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I}, // 25
+                                        {I,o,o,o,o,I,I,O,o,o,o,I,e,e,e,I,v,I,e,e,e,I,o,o,o,o,o,O,o,o,I},
+                                        {I,I,I,I,I,I,I,I,I,I,I,I,e,e,e,I,v,I,e,e,e,I,I,I,I,I,I,I,I,I,I}};
+    std::pair<int, int> pos = {14, 7};
+    int pelletsLeft = 50;
+    int pellets_eat = 0;
+    int prev_powerups = 0;
+    int curr_powerup = 0;
+    int cherries_eat = 0;
+    PacmanState pac(pos, PacmanState::Direction::up, grid, pelletsLeft, pellets_eat, cherries_eat, prev_powerups, curr_powerup);
+
+    std::pair<int, int> ghostLoc = {1, 1};
+    std::pair<int, int> scatterLoc = {25, 32};
+    Ghost ghost(ghostLoc, scatterLoc, pos, Ghost::Direction::up, Ghost::GhostState::chase);
+
+    // if (pac.getDirection() == PacmanState::Direction::left) {
+    //     std::cout << "you chillin" << std::endl;
+    // }
+    
+    // std::cout << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << std::endl;
     // std::cout << pac.getPos().first << " " << pac.getPos().second << std::endl;
     // Ghost ghost();
     // std::cout << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << std::endl;
@@ -38,18 +84,18 @@ int main () {
 
 
 
-    ghost.ghostMove(std::pair<int, int>(1,1));
-    // std::cout << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << std::endl;
-    // std::cout << pac.getPos().first << " " << pac.getPos().second << std::endl;
+    // ghost.ghostMove(std::pair<int, int>(1,1));
+    // // std::cout << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << std::endl;
+    // // std::cout << pac.getPos().first << " " << pac.getPos().second << std::endl;
 
-    // (ghost.getGhostLocation().first != pac.getPos().first) || (ghost.getGhostLocation().second != pac.getPos().second)
-    int count = 100;
-    while (count > 0) {
-        std::cout << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << " " << ghost.get_euclidian_distance(ghost.getGhostLocation(), pac.getPos()) << std::endl;
+    // // (ghost.getGhostLocation().first != pac.getPos().first) || (ghost.getGhostLocation().second != pac.getPos().second)
+    // int count = 100;
+    while (pac.getBotPos().first != ghost.getGhostLocation().first || pac.getBotPos().second != ghost.getGhostLocation().second) {
+        std::cout << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << " " << ghost.get_euclidian_distance(ghost.getGhostLocation(), pac.getBotPos()) << std::endl;
         // std::cout << ghost._get_next_red_chase_move().first.first << " " << ghost._get_next_red_chase_move().first.second << std::endl;
-        std::pair<int, int> newMove(ghost._get_next_orange_chase_move().first.first, ghost._get_next_orange_chase_move().first.second);
+        std::pair<int, int> newMove(ghost._get_next_red_chase_move(pac.getBotPos(), pac.getDirection(), ghost.getGhostLocation(), ghost.getterDirection()).first.first, ghost._get_next_red_chase_move(pac.getBotPos(), pac.getDirection(), ghost.getGhostLocation(), ghost.getterDirection()).first.second);
         ghost.ghostMove(newMove);
-        --count;
     }
+    std::cout << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << std::endl;
     return 0;
 }
