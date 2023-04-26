8252c189716c3076948eef531ba07ed42b4b648b
Tamir Battsogt <angutamir@gmail.com>
2023-04-26 18:52:17 -0500

    m

--- bot_code/high_level/PacmanState.cpp
+++ bot_code/high_level/PacmanState.cpp
@@ -1,97 +1,97 @@
 #include "PacmanState.h"
 
 //************************ Constructor and Destructor
-PacmanState::PacmanState() {
-}
+// PacmanState::PacmanState() {
+// }
 
-PacmanState::PacmanState(std::pair<int, int> botPos, PacmanState::Direction pacDirection, std::vector<std::vector<int>> grid, int pelletsLeft, int pellets_eat, int cherries_eat, int powerups) {
-     {
-        // score = 0; // retrieve from server
-        // lives = 3; // retrieve from server
-        gameover = false; // retrieve from server
-        pos = botPos; // retrieve from server
-        direction_facing = pacDirection;
-        pellets_left = pelletsLeft; // retrieve from server
-        pellets_eaten = pellets_eat; // retrieve from server
-        cherries_eaten = cherries_eat; // retrieve from server
-        powerups_eaten = powerups; // retrieve from server
-    }
-}
+// PacmanState::PacmanState(std::pair<int, int> botPos, PacmanState::Direction pacDirection, std::vector<std::vector<int>> grid, int pelletsLeft, int pellets_eat, int cherries_eat, int powerups) {
+//      {
+//         // score = 0; // retrieve from server
+//         // lives = 3; // retrieve from server
+//         gameover = false; // retrieve from server
+//         pos = botPos; // retrieve from server
+//         direction_facing = pacDirection;
+//         pellets_left = pelletsLeft; // retrieve from server
+//         pellets_eaten = pellets_eat; // retrieve from server
+//         cherries_eaten = cherries_eat; // retrieve from server
+//         powerups_eaten = powerups; // retrieve from server
+//     }
+// }
 
-PacmanState::PacmanState(PacmanState & state) {
-    pos = state.pos;
-    direction_facing = state.direction_facing;
-    grid = state.grid;
-}
+// PacmanState::PacmanState(PacmanState & state) {
+//     pos = state.pos;
+//     direction_facing = state.direction_facing;
+//     grid = state.grid;
+// }
 //************************ Constructor and Destructor
 //************************ Position getter and setter
-std::pair<int, int> PacmanState::getBotPos() {
-    return pos;
-}
+// std::pair<int, int> PacmanState::getBotPos() {
+//     return pos;
+// }
 
-std::vector<std::pair<int, int>> PacmanState::find_possible_moves() {
+// std::vector<std::pair<int, int>> PacmanState::find_possible_moves() {
         
-        std::pair<int, int> up = std::pair<int, int>(pos.first, pos.second + 1); //up
-        std::pair<int, int> left = std::pair<int, int>(pos.first - 1, pos.second); //left
-        std::pair<int, int> down = std::pair<int, int>(pos.first, pos.second - 1); //down
-        std::pair<int, int> right = std::pair<int, int>(pos.first + 1, pos.second); //right
+//         std::pair<int, int> up = std::pair<int, int>(pos.first, pos.second + 1); //up
+//         std::pair<int, int> left = std::pair<int, int>(pos.first - 1, pos.second); //left
+//         std::pair<int, int> down = std::pair<int, int>(pos.first, pos.second - 1); //down
+//         std::pair<int, int> right = std::pair<int, int>(pos.first + 1, pos.second); //right
 
-        std::vector<std::pair<int, int>> possible;
+//         std::vector<std::pair<int, int>> possible;
 
-        if (is_move_legal(up)) {
-            possible.push_back(up);
-        }
-        if (is_move_legal(left)) {
-            possible.push_back(left);
-        }
-        if (is_move_legal(down)) {
-            possible.push_back(down);
-        }
-        if (is_move_legal(right)) {
-            possible.push_back(right);
-        }
-        return possible;
-    }
+//         if (is_move_legal(up)) {
+//             possible.push_back(up);
+//         }
+//         if (is_move_legal(left)) {
+//             possible.push_back(left);
+//         }
+//         if (is_move_legal(down)) {
+//             possible.push_back(down);
+//         }
+//         if (is_move_legal(right)) {
+//             possible.push_back(right);
+//         }
+//         return possible;
+//     }
 
-    bool PacmanState::is_move_legal(std::pair<int, int> move) {
-        return (move != pos && grid[move.first][move.second] != I && 
-        grid[move.first][move.second] != n && 
-        grid[move.first][move.second]  != e);
-    }
+    // bool PacmanState::is_move_legal(std::pair<int, int> move) {
+    //     return (move != pos && grid[move.first][move.second] != I && 
+    //     grid[move.first][move.second] != n && 
+    //     grid[move.first][move.second]  != e);
+    // }
 
-void PacmanState::move(std::pair<int, int> move) {
-    PacmanState::Direction facing = getDirection(pos, move);
-    if (facing == PacmanState::up) {
-        direction_facing = Ghost::up;
-    } else if (facing == Ghost::left) {
-        direction_facing = Ghost::left;
-    } else if (facing == Ghost::right) {
-        direction_facing = Ghost::right;
-    } else if (facing == Ghost::down) {
-        direction_facing = Ghost::down;
-    }
+// void PacmanState::move(std::pair<int, int> move) {
+//     PacmanState::Direction facing = getDirection(pos, move);
+//     if (facing == PacmanState::up) {
+//         direction_facing = Ghost::up;
+//     } else if (facing == Ghost::left) {
+//         direction_facing = Ghost::left;
+//     } else if (facing == Ghost::right) {
+//         direction_facing = Ghost::right;
+//     } else if (facing == Ghost::down) {
+//         direction_facing = Ghost::down;
+//     }
 
-    if (grid[move.first][move.second] == 2 || grid[move.first][move.second] == 4) {
-        grid[move.first][move.second] = 7;
-    }
-    pos = move;
-}
+//     if (grid[move.first][move.second] == 2 || grid[move.first][move.second] == 4) {
+//         grid[move.first][move.second] = 7;
+//     }
+//     pos = move;
+// }
 
 
-PacmanState::Direction PacmanState::getDirection(std::pair<int, int> prevPos, std::pair<int, int> newPos) {
-        if (newPos.first > prevPos.first) {
-            return PacmanState::Direction::right;
-        } else if (newPos.first < prevPos.first) {
-            return PacmanState::Direction::left;
-        } else if (newPos.second > prevPos.second) {
-            return PacmanState::Direction::up;
-        } else if (newPos.second < prevPos.second) {
-            return PacmanState::Direction::down;
-        }
-        else {
-            return direction_facing;
-        }
-    }
+// PacmanState::Direction PacmanState::getDirection(std::pair<int, int> prevPos, std::pair<int, int> newPos) {
+//         if (newPos.first > prevPos.first) {
+//             return PacmanState::Direction::right;
+//         } else if (newPos.first < prevPos.first) {
+//             return PacmanState::Direction::left;
+//         } else if (newPos.second > prevPos.second) {
+//             return PacmanState::Direction::up;
+//         } else if (newPos.second < prevPos.second) {
+//             return PacmanState::Direction::down;
+//         }
+//         else {
+//             return direction_facing;
+//         }
+//     }
 //************************
 
 // void PacmanState::movePlayer(std::pair<int, int> move) {
--- bot_code/high_level/test.cpp
+++ bot_code/high_level/test.cpp
@@ -34,46 +34,40 @@ int main () {
                                         {I,o,I,I,o,I,I,o,I,I,o,I,e,e,e,I,v,I,e,e,e,I,o,I,I,o,I,I,I,o,I}, // 25
                                         {I,o,o,o,o,I,I,O,o,o,o,I,e,e,e,I,v,I,e,e,e,I,o,o,o,o,o,O,o,o,I},
                                         {I,I,I,I,I,I,I,I,I,I,I,I,e,e,e,I,v,I,e,e,e,I,I,I,I,I,I,I,I,I,I}};
-
-
-    vector<vector<int>> testgrid = {{I, I, I, I, I},
-                                    {I, o, o, o, I},
-                                    {I, o, I, o, I},
-                                    {I, o, I, o, I},
-                                    {I, I, I, I, I}};                                   
-    std::pair<int, int> pos = {3, 1};
+                                  
+    std::pair<int, int> pos = {14, 7};
     int pelletsLeft = 50;
     int pellets_eat = 0;
     int prev_powerups = 0;
     int curr_powerup = 0;
     int cherries_eat = 0;
-    PacmanState pac(pos, PacmanState::Direction::right, testgrid, pelletsLeft, pellets_eat, cherries_eat, curr_powerup);
+    PacmanState pac(pos, PacmanState::Direction::right, grid, cherries_eat, curr_powerup);
 
     std::pair<int, int> ghostLoc = {1, 1};
-    std::pair<int, int> scatterLoc = {1, 1};
-    Ghost ghost(ghostLoc, scatterLoc, pos, Ghost::Direction::up, Ghost::GhostState::chase);
+    // std::pair<int, int> scatterLoc = {, 1};
+    Ghost ghost(ghostLoc, Ghost::Direction::up, Ghost::Color::red, Ghost::GhostState::chase);
 
-    Ghost ghost_blue(ghostLoc, scatterLoc, pos, Ghost::Direction::up, Ghost::GhostState::chase);
+    Ghost ghost_blue(ghostLoc, Ghost::Direction::up, Ghost::Color::blue, Ghost::GhostState::chase);
 
-    std::cout << "Blue" << " " << ghost_blue.getGhostLocation().first << " " << ghost_blue.getGhostLocation().second << " " << ghost_blue.get_euclidian_distance(ghost_blue.getGhostLocation(), pac.getBotPos()) << std::endl;
-    std::cout << "Red" << " " << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << " " << ghost.get_euclidian_distance(ghost.getGhostLocation(), pac.getBotPos()) << std::endl;
+    // std::cout << "Blue" << " " << ghost_blue.getGhostLocation().first << " " << ghost_blue.getGhostLocation().second << " " << ghost_blue.get_euclidian_distance(ghost_blue.getGhostLocation(), pac.getBotPos()) << std::endl;
+    // std::cout << "Red" << " " << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << " " << ghost.get_euclidian_distance(ghost.getGhostLocation(), pac.getBotPos()) << std::endl;
 
 
-    std::pair<int, int> newMoveRed(ghost._get_next_red_chase_move(pac.getBotPos(), pac.getDirection(), ghost.getGhostLocation(), ghost.getterDirection()).first.first, ghost._get_next_red_chase_move(pac.getBotPos(), pac.getDirection(), ghost.getGhostLocation(), ghost.getterDirection()).first.second);
-    std::pair<int, int> newMove(ghost_blue._get_next_blue_chase_move(pac.getBotPos(), pac.getDirection(), ghost_blue.getGhostLocation(), ghost.getGhostLocation()).first.first, ghost_blue._get_next_blue_chase_move(pac.getBotPos(), pac.getDirection(), ghost_blue.getGhostLocation(), ghost.getGhostLocation()).first.second);
-    ghost_blue.ghostMove(newMove);
-    ghost.ghostMove(newMoveRed);
+    // std::pair<int, int> newMoveRed(ghost._get_next_red_chase_move(pac.getBotPos(), pac.getDirection(), ghost.getGhostLocation(), ghost.getterDirection()).first.first, ghost._get_next_red_chase_move(pac.getBotPos(), pac.getDirection(), ghost.getGhostLocation(), ghost.getterDirection()).first.second);
+    // std::pair<int, int> newMove(ghost_blue._get_next_blue_chase_move(pac.getBotPos(), pac.getDirection(), ghost_blue.getGhostLocation(), ghost.getGhostLocation()).first.first, ghost_blue._get_next_blue_chase_move(pac.getBotPos(), pac.getDirection(), ghost_blue.getGhostLocation(), ghost.getGhostLocation()).first.second);
+    // ghost_blue.ghostMove(newMove);
+    // ghost.ghostMove(newMoveRed);
 
-    std::cout << "Blue" << " " << ghost_blue.getGhostLocation().first << " " << ghost_blue.getGhostLocation().second << " " << ghost_blue.get_euclidian_distance(ghost_blue.getGhostLocation(), pac.getBotPos()) << std::endl;
-    std::cout << "Red" << " " << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << " " << ghost.get_euclidian_distance(ghost.getGhostLocation(), pac.getBotPos()) << std::endl;
+    // std::cout << "Blue" << " " << ghost_blue.getGhostLocation().first << " " << ghost_blue.getGhostLocation().second << " " << ghost_blue.get_euclidian_distance(ghost_blue.getGhostLocation(), pac.getBotPos()) << std::endl;
+    // std::cout << "Red" << " " << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << " " << ghost.get_euclidian_distance(ghost.getGhostLocation(), pac.getBotPos()) << std::endl;
 
-    std::pair<int, int> newMoveRedTwo(ghost._get_next_red_chase_move(pac.getBotPos(), pac.getDirection(), ghost.getGhostLocation(), ghost.getterDirection()).first.first, ghost._get_next_red_chase_move(pac.getBotPos(), pac.getDirection(), ghost.getGhostLocation(), ghost.getterDirection()).first.second);
-    std::pair<int, int> newMoveTwo(ghost_blue._get_next_blue_chase_move(pac.getBotPos(), pac.getDirection(), ghost_blue.getGhostLocation(), ghost.getGhostLocation()).first.first, ghost_blue._get_next_blue_chase_move(pac.getBotPos(), pac.getDirection(), ghost_blue.getGhostLocation(), ghost.getGhostLocation()).first.second);
-    ghost_blue.ghostMove(newMoveTwo);
-    ghost.ghostMove(newMoveRedTwo);
+    // std::pair<int, int> newMoveRedTwo(ghost._get_next_red_chase_move(pac.getBotPos(), pac.getDirection(), ghost.getGhostLocation(), ghost.getterDirection()).first.first, ghost._get_next_red_chase_move(pac.getBotPos(), pac.getDirection(), ghost.getGhostLocation(), ghost.getterDirection()).first.second);
+    // std::pair<int, int> newMoveTwo(ghost_blue._get_next_blue_chase_move(pac.getBotPos(), pac.getDirection(), ghost_blue.getGhostLocation(), ghost.getGhostLocation()).first.first, ghost_blue._get_next_blue_chase_move(pac.getBotPos(), pac.getDirection(), ghost_blue.getGhostLocation(), ghost.getGhostLocation()).first.second);
+    // ghost_blue.ghostMove(newMoveTwo);
+    // ghost.ghostMove(newMoveRedTwo);
 
-    std::cout << "Blue" << " " << ghost_blue.getGhostLocation().first << " " << ghost_blue.getGhostLocation().second << " " << ghost_blue.get_euclidian_distance(ghost_blue.getGhostLocation(), pac.getBotPos()) << std::endl;
-    std::cout << "Red" << " " << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << " " << ghost.get_euclidian_distance(ghost.getGhostLocation(), pac.getBotPos()) << std::endl;
+    // std::cout << "Blue" << " " << ghost_blue.getGhostLocation().first << " " << ghost_blue.getGhostLocation().second << " " << ghost_blue.get_euclidian_distance(ghost_blue.getGhostLocation(), pac.getBotPos()) << std::endl;
+    // std::cout << "Red" << " " << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << " " << ghost.get_euclidian_distance(ghost.getGhostLocation(), pac.getBotPos()) << std::endl;
     // if (pac.getDirection() == PacmanState::Direction::left) {
     //     std::cout << "you chillin" << std::endl;
     // }
@@ -141,12 +135,12 @@ int main () {
     //ghost.ghostMove(std::pair<int, int>(1,1));
     // std::cout << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << std::endl;
     // std::cout << pac.getPos().first << " " << pac.getPos().second << std::endl;
-    //while ((ghost.getGhostLocation().first != pac.getPos().first) || (ghost.getGhostLocation().second != pac.getPos().second)) {
-        //std::cout << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << std::endl;
-        // std::cout << ghost._get_next_red_chase_move().first.first << " " << ghost._get_next_red_chase_move().first.second << std::endl;
-        //std::pair<int, int> newMove(ghost._get_next_red_chase_move().first.first, ghost._get_next_red_chase_move().first.second);
-        //ghost.ghostMove(newMove);
-    //}
+    while ((ghost.getGhostLocation().first != pac.getBotPos().first) || (ghost.getGhostLocation().second != pac.getBotPos().second)) {
+        std::cout << ghost.getGhostLocation().first << " " << ghost.getGhostLocation().second << std::endl;
+        // std::cout << ghost._get_next_red_chase_move(pac.getBotPos()).first.first << " " << ghost._get_next_red_chase_move(pac.getBotPos()).first.second << std::endl;
+        std::pair<int, int> newMove(ghost._get_next_red_chase_move(pac.getBotPos()).first.first, ghost._get_next_red_chase_move(pac.getBotPos()).first.second);
+        ghost.ghostMove(newMove);
+    }
 
     // this is for testing the hashing 
     //run g++ test.cpp Hashing_shit.cpp -o testing
