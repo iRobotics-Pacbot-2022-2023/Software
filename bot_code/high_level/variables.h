0dbe578347fdfba02c059fb7ab40d5ff8129fe6f
Rohan G <gvrohan11@gmail.com>
2024-04-21 18:56:26 -0500

    fixed errors

--- bot_code/high_level/variables.h
+++ bot_code/high_level/variables.h
@@ -10,26 +10,31 @@ using namespace std;
 // Variables
 
 // Game params
-const pair<int,int> PACBOT_STARTING_POSITION = {14, 7};
+const pair<int,int> PACBOT_STARTING_POSITION(14, 7);
 
 // Cherry param
-const pair<int,int> CHERRY_POS = {13, 13};
+const pair<int,int> CHERRY_POS(13, 13);
 
 // Ghost Exit
-const pair<float,int> GHOST_EXIT = {13.5, 19};
+const pair<float,int> GHOST_EXIT(13.5, 19);
 
 // Ghost starting positions
-const pair<int,int> RED_GHOST_STARTING_POSITION = {13, 11};
-const pair<int,int> PINK_GHOST_STARTING_POSITION = {13, 12};
-const pair<int,int> BLUE_GHOST_STARTING_POSITION = {13, 13};
-const pair<int,int> ORANGE_GHOST_STARTING_POSITION = {13, 14};
-const pair<int,int> GHOST_STARTING_POSITIONS[4] = {{13, 11}, {13, 12}, {13, 13}, {13, 14}};
+const pair<int,int> RED_GHOST_STARTING_POSITION(13, 11);
+const pair<int,int> PINK_GHOST_STARTING_POSITION(13, 12);
+const pair<int,int> BLUE_GHOST_STARTING_POSITION(13, 13);
+const pair<int,int> ORANGE_GHOST_STARTING_POSITION(13, 14);
+const pair<int, int> GHOST_STARTING_POSITIONS[4] = {
+    pair<int, int>(13, 11),
+    pair<int, int>(13, 12),
+    pair<int, int>(13, 13),
+    pair<int, int>(13, 14)
+};
 
 // Ghost scatter locations
-const pair<int, int> RED_GHOST_SCATTER_LOCATION = {1, 1};
-const pair<int, int> PINK_GHOST_SCATTER_LOCATION = {1, 19};
-const pair<int, int> BLUE_GHOST_SCATTER_LOCATION = {26, 1};
-const pair<int, int> ORANGE_GHOST_SCATTER_LOCATION = {26, 19};
+const pair<int, int> RED_GHOST_SCATTER_LOCATION(1, 1);
+const pair<int, int> PINK_GHOST_SCATTER_LOCATION(1, 19);
+const pair<int, int> BLUE_GHOST_SCATTER_LOCATION(26, 1);
+const pair<int, int> ORANGE_GHOST_SCATTER_LOCATION(26, 19);
 // const pair<int,int> GHOST_SCATTER_LOCATIONS[4] = {{1, 1}, {1, 19}, {26, 1}, {26, 19}};
 
 // Grid Parameters
